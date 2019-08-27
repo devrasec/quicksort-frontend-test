4972b54a31721b5066278d99704d5f1d00e0b3ff
Luis Contreras <one.tmac@gmail.com>
2019-08-26 23:44:03 -0400

    [feat] Add drawing shapes functionality

--- src/components/ShapesRenderer/ShapesRenderer.js
+++ src/components/ShapesRenderer/ShapesRenderer.js
@@ -1,11 +1,22 @@
 import React, { useRef, useState, useEffect } from 'react';
 // eslint-disable-next-line no-unused-vars
 import styled from 'styled-components/macro';
-import { drawPoint, getMissingPoint } from './utils';
+import {
+  drawPoint,
+  getMissingPoint,
+  drawParallelogram,
+  getParallelogramArea,
+  drawCircle,
+  getRadioByArea,
+  getCenterCoords
+} from './utils';
 
 export const ShapesRenderer = () => {
   const canvasRef = useRef(null);
   const [points, setPoints] = useState([]);
+  const [area, setArea] = useState(0);
+  const { current: canvas } = canvasRef;
+  const isCanvasAvailable = canvas && canvas.getContext;
 
   const addPoint = point => setPoints(prevPoints => [...prevPoints, point]);
 
@@ -15,14 +26,29 @@ export const ShapesRenderer = () => {
       return;
     }
 
-    const { current: canvas } = canvasRef;
-
-    if (canvas && canvas.getContext) {
+    if (isCanvasAvailable) {
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, window.innerHeight, window.innerWidth);
       points.forEach(point => drawPoint(ctx, point));
+
+      if (points.length === 4) {
+        drawParallelogram(ctx, points);
+        setArea(getParallelogramArea(points));
+      }
     }
-  }, [points]);
+  }, [canvas, isCanvasAvailable, points]);
+
+  useEffect(() => {
+    if (points.length === 4 && area > 0) {
+      if (isCanvasAvailable) {
+        drawCircle(
+          canvas.getContext('2d'),
+          getCenterCoords(points),
+          getRadioByArea(area)
+        );
+      }
+    }
+  }, [area, canvas, isCanvasAvailable, points]);
 
   const handleCanvasClick = e => {
     if (points.length < 3) {
--- src/components/ShapesRenderer/utils.js
+++ src/components/ShapesRenderer/utils.js
@@ -1,8 +1,8 @@
-export const drawPoint = (ctx, location) => {
-  ctx.fillStyle = '#ff0000';
-  ctx.beginPath();
-  ctx.arc(location.x, location.y, 5.5, 0, 2 * Math.PI);
-  ctx.fill();
+export const drawPoint = (canvasCtx, location) => {
+  canvasCtx.fillStyle = '#ff0000';
+  canvasCtx.beginPath();
+  canvasCtx.arc(location.x, location.y, 5.5, 0, 2 * Math.PI);
+  canvasCtx.fill();
 };
 
 /**
@@ -20,4 +20,65 @@ export const getMissingPoint = points => {
     x: a.x + c.x - b.x,
     y: a.y + c.y - b.y
   };
+};
+
+export const drawParallelogram = (canvasCtx, points) => {
+  canvasCtx.strokeStyle = '#0000ff';
+  canvasCtx.lineWidth = 5;
+  canvasCtx.lineJoin = 'round';
+  canvasCtx.beginPath();
+  canvasCtx.moveTo(points[0].x, points[0].y);
+  canvasCtx.lineTo(points[1].x, points[1].y);
+  canvasCtx.lineTo(points[2].x, points[2].y);
+  canvasCtx.lineTo(points[3].x, points[3].y);
+  canvasCtx.lineTo(points[0].x, points[0].y);
+  canvasCtx.stroke();
+};
+
+/**
+ * Return the area of the parallelogram given its four vertices
+ * @see http://faculty.bard.edu/belk/math213/Determinants.pdf
+ * @param array points Array of parallelogram vertices.
+ */
+export const getParallelogramArea = points => {
+  const [a, b, c] = points;
+  // Determinant of two adjacents sides of the parallelogram.
+  const determinant = [[a.x - b.x, a.y - b.y], [b.x - c.x, b.y - c.y]];
+  // Cross product of the determinant is equal to the area.
+  return Math.abs(
+    determinant[0][0] * determinant[1][1] -
+      determinant[1][0] * determinant[0][1]
+  );
+};
+
+export const getCenterCoords = points => {
+  const sumX = sumBy(points, point => point.x);
+
+  const sumY = sumBy(points, point => point.y);
+
+  return {
+    x: sumX / 4,
+    y: sumY / 4
+  };
+};
+
+export const getRadioByArea = area => {
+  // The formula of the circle area is A = PI * r2
+  return Math.sqrt(area / Math.PI);
+};
+
+export const drawCircle = (ctx, point, radio) => {
+  ctx.strokeStyle = 'yellow';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(point.x, point.y, radio, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+const sumBy = (arr, getValue) => {
+  if (!Array.isArray(arr)) return NaN;
+
+  return arr.reduce((acc, current) => {
+    return acc + Number(getValue(current));
+  }, 0);
 };
